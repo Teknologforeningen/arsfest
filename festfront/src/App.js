9a735695a15ef5393fea4dea6b2494fa28349c8d
Carl-Victor Schauman <cv.schauman@gmail.com>
2022-01-27 17:22:19 +0200

    Routing and Historik added.

--- festfront/src/App.js
+++ festfront/src/App.js
@@ -1,10 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Home from './Views/Home';
+import Historik from './Views/Historik';
 
 function App() {
   return (
-    < Home />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/historik" element={<Historik />}/>
+          {//<Route path="/anmalan" component={deltagaranmalan}/>
+          //<Route path="/fotoinfo" component={fotoinfo}/>
+          //<Route path="/coronainfo" component={coronainfo}/>
+          //<Route path="/fundraising" component={fundraising}/>
+          //<Route path="/vettoetikett" component={vettoetikett}/>
+          }
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ festfront/src/Views/Historik.js
@@ -0,0 +1,127 @@
+const Historik = () => {
+    return (
+        <>
+        <div class="center-page">
+            <div class="mobile-header">
+            <img id="navMenuIcon" src="./assets/menu.svg" class="menu"></img>
+            <a href="./index.html">
+                <h1 class="mobile-nav-header">
+                TF 150
+                </h1>
+            </a>
+            </div>
+            <div id="mobileNav" class="mobile-nav-container">
+            <img id="navMenuClose" src="./assets/close.svg" class="menu-close"></img>
+            <ul class="nav-ul">
+                <li class="nav-li">
+                <a href="./registration.html">Deltagaranmälan</a>
+                </li>
+                <li class="nav-li">
+                <a href="./index.html">Basinfo</a>
+                </li>
+                <li class="nav-li">
+                <a href="./photoinfo.html">Fotoinfo</a>
+                </li>
+                <li class="nav-li">
+                <a href="./coronainfo.html">Coronainfo/FAQ</a>
+                </li>
+                <li class="nav-li">
+                <a href="./fundraising.html">Fundraising</a>
+                </li>
+                <li class="nav-li">
+                <a href="./historik.html">Historik</a>
+                </li>
+                <li class="nav-li">
+                <a href="./vettoetikett.html">Vett och Etikett</a>
+                </li>
+            </ul>
+            </div>
+            <nav class="nav">
+            <a href="./index.html">
+                <h1 class="nav-header">
+                TF
+                <br/>
+                150
+                </h1>
+            </a>
+            <ul class="nav-ul">
+                <li class="nav-li">
+                <a href="./registration.html">Deltagaranmälan</a>
+                </li>
+                <li class="nav-li">
+                <a href="./index.html">Basinfo</a>
+                </li>
+                <li class="nav-li">
+                <a href="./photoinfo.html">Fotoinfo</a>
+                </li>
+                <li class="nav-li">
+                <a href="./coronainfo.html">Coronainfo/FAQ</a>
+                </li>
+                <li class="nav-li">
+                <a href="./fundraising.html">Fundraising</a>
+                </li>
+                <li class="nav-li">
+                <a href="./historik.html">Historik</a>
+                </li>
+                <li class="nav-li">
+                <a href="./vettoetikett.html">Vett och Etikett</a>
+                </li>
+            </ul>
+            </nav>
+            <div class="page-content">
+            <div class="page-content-container">
+                <div class="page-content-center">
+                <h2 class="page-content-title">TF:s historik</h2>
+                <p class="page-content-text">
+                    Jubileumsåret till ära har nationen producerat en 150-årshistorik — en högkvalitativ och verklighetsförankrad dokumentation av TF genom tiderna.
+                </p>
+        
+                <p class="page-content-text">
+                    Teknologföreningen är grundad i Helsingfors år 1872. TF har sedan dess danat svenskspråkiga samt personer med intresse för svenskan 
+                    från hela Finland. Alla, från Pargas övärld till Österbottens slätter och Helsingfors betonggårdar, har tagits emot med öppna armar. 
+                    Både TF och läroverket som kom att bli Tekniska Högskolan (och senare Aalto-universitetet) växte fram och utvecklades tillsammans med 
+                    Finland. Nationen var hemmastadd i Helsingfors från grundandet fram till 1966, då Tekniska Högskolans kampus flyttade till Otnäs i Esbo. 
+                </p>
+        
+                <p class="page-content-text">
+                    Det är inte heller enbart i den akademiska världen som TF satt sitt spår. På 1960-talet bifogades Paavo Nurmi-statyn till Vasa-skeppets 
+                    historia (däribland en TF:are, så klart) och på 1980-talet blev TF Disco känt i hela Finland. År 2005 stod TF värd för efterfesterna under 
+                    VM i friidrott och nationen har under den senaste tiden aktivt varit med i utvecklingen av Victoria-kvarteret på Busholmen, där TF idag 
+                    handharstudiebostäder.
+                </p>
+        
+                <p class="page-content-text">
+                    Historiken bygger förståelse för TF:s och hela teknologsamfundets utveckling från 1870-talet till dagens värld. Historiken publiceras i samband med Teknologföreningens 150-års jubileumsårsfest i mars 2022.
+                </p>
+        
+                <p class="content-list">
+                    <b>Priser</b>
+                </p>
+                <p class="content-list">
+                    Normalpris: 35 €
+                </p>
+                <p class="content-list">
+                    Studerandepris: 20 €
+                </p>
+                <p class="content-list">
+                    TF Stälm / TFiF-medlem: 28 €
+                </p>
+                <p class="content-list">
+                    Understödspris: 150 €
+                </p>
+                <p class="page-content-text">
+                    Beställ din historik via <a href="https://shop.tf.fi">shop.tf.fi</a> 
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
+        <img src="./assets/top_left.svg" class="img-top-left"></img>
+        <img src="./assets/bot_left.svg" class="img-bottom-left"></img>
+        <img src="./assets/top_right.svg" class="img-top-right"></img>
+        <img src="./assets/bot_right.svg" class="img-bottom-right"></img>
+        </>
+    )
+}
+
+export default Historik;
