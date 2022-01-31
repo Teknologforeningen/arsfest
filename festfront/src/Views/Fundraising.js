3e642a7423b5759a40e4af157d1735a5fa886271
Carl-Victor Schauman <cv.schauman@gmail.com>
2022-01-31 13:26:36 +0200

    Fundraising text update

--- festfront/src/Views/Fundraising.js
+++ festfront/src/Views/Fundraising.js
@@ -10,33 +10,53 @@ const Fundraising = () => {
         <div className="page-content">
         <div className="page-content-container"> 
             <div className="page-content-center">
-            <h2 className="page-content-title">Hjälp oss bygga ett nytt hem för TF och hållbart bevara TF-andan också de följande 150 åren - var med och bygga träffpunkt Aalto!</h2>
+            <h2 className="page-content-title">Hjälp oss bygga ett nytt hem för TF och hållbart bevara TF-andan också de följande 150 åren - var med och bygga Träffpunkt Aalto!</h2>
+            
+            <p className="page-content-text">
+                <b>Du kan läsa mera om TF:s nya nationshus och bekanta dig med de som hittills deltagit i insamlingen på <u><a href="https://vision.tf.fi">vision.tf.fi</a></u>. 
+                    Mera information och svar på frågor ger Fundraisingchefen på TF, Emil Kauppi (+358 45 6444 599, <a href="mailto: emil.kauppi@tf.fi">emil.kauppi@tf.fi)</a>. 
+                    Ifall du har vidare tankar kring ditt deltagande så var modigt i kontakt. </b>
+            </p>
+
             <p className="content-list">
-                <b>Vi är på god väg – nu behövs ni alla! <a href="https://vision.tf.fi">vision.tf.fi</a></b>
+                <b>Medelinsamlingen har kommit bra igång</b>
             </p>
             <p className="page-content-text">
-                Teknologföreningen räknar med 4M€ i insamlade medel, utöver de insparade medel som nationen samlat på sig, inför flytten till sitt nya hem. Vi är redan nu på god väg <b>och har säkrat 3,0M€</b>. Nu riktar vi oss till alla våra StÄlMar, forna TF:are och TF-vänner, och utmanar er att komma med en egen kontribution för att säkra den återstående miljonen och göra detta till ett projekt till en gemensam insats! 
-            </p>
-    
-            <p className="page-content-text">
-                StÄlMar, alumner och övriga TF-vänner har en nyckelroll i att förverkliga de unika och ändamålsenliga medlemsutrymmen som TF behöver. I framtida Otnäs kommer TF att behöva ett vardagsrum som bidrar till ett synligt TF och som i större grad gynnar gemenskapen vid Aalto-universitetet. Detta utan att glömma bort fest, tjohej och traditioner. För en bättre framtid för TF, Aalto-universitet och Otnäs: var med och skapa Träffpunkt Aalto – ett öppet, levande och hållbart nationshus med Otnäs bästa läge!
-            </p>
-    
-            <p className="content-list"><b>Vi bygger vidare från en exemplarisk grundplåt</b></p>
-            <p className="page-content-text">
-                Hittills har vi fått ta del av otroligt fina bidrag från finlandssvenska stiftelser då redan åtta stiftelser bidragit till Träffpunkt Aalto-projektet med hela 2,6M€. Detta är något vi inte ser som självklart och vi är otroligt tacksamma för dessa bidrag! Vi är därmed övertygande om att projektet kommer att lämna stora spår efter sig för enskilda TF:aren, TF, universitetsmiljön och samhället.
+                TF:s totala investering i flytten till det nya hemmet kommer kosta 6,8 M€, av vilka 4M€ kommer från medelinsamlingen och resten 
+                från befintliga medel. Medelinsamlingen har kommit bra igång och <b>vi har redan säkrat 3,0M€ i bidrag!</b> 
             </p>
     
             <p className="page-content-text">
-                Utöver stiftelserna har vi redan under förra hösten fått ta del av kring 60 bidrag från storhjärtade StÄlMar och TF-vänner som vill vara med och se nationen leva vidare och ge tillbaka så TF:are i framtiden har lika bra förutsättningar som de själva haft. <b>Den här gruppen har exemplariskt bidragit med en kraftig grundplåt på ungefär 500K €. För gruppen har en månadslön och flera procent av nettoegendomen varit resonemanget bakom summorna</b>. Bland dessa exempelvisande individer finns både StÄlMar och icke-StÄlMar, “gamla” och “unga”, …. Nu är det din tur! 
+                Det otroligt fina är att åtta stycken finlandssvenska stiftelser bidragit till Träffpunkt Aalto-projektet med hela 2,6 M€. 
+                Vi är otroligt tacksamma för dessa bidrag! Vi är därmed övertygande om att projektet kommer att kunna genomföras med heder 
+                och den finlandssvenska kulturen kommer höras och synas vid Aalto-universitetet även i framtiden.
             </p>
     
             <p className="page-content-text">
-                Du kan läsa mera om TF:s nya nationshus och bekanta dig med de som hittills deltagit i insamlingen på  <a href="https://vision.tf.fi">vision.tf.fi</a>. Mera information och svar på frågor ger 
+                Därtill har vi under höstens lopp fått ta del av kring 60 bidrag från storhjärtade StÄlMar och TF-vänner som vill vara med och se 
+                till att nationen kan bibehålla sin livskraft och ge kommande TF-generationer lika goda förutsättningar att förverkliga sig själva 
+                och utvecklas, som de själva haft under sin studietid.
             </p>
     
             <p className="page-content-text">
-                Fundraisingchefen på TF, Emil Kauppi (+358 45 6444 599, <a href="mailto: emil.kauppi@tf.fi">emil.kauppi@tf.fi)</a>. Ifall du har vidare tankar kring ditt deltagande så var modigt i kontakt med Emil. 
+                <b>Den här gruppen har exemplariskt bidragit med en kraftig grundplåt på ungefär 500 k€. För gruppen har en bruttomånadslön och flera 
+                procent av nettoegendomen varit tankegången bakom summorna.</b> Bland dessa exempelvisande individer finns både StÄlMar och icke-StÄlMar, 
+                äldre och yngre TF-själar. Nu är det din tur att hänga med! 
+            </p>
+
+            <p className="content-list">
+                <b>Ditt bidrag är viktigt</b>
+            </p>
+    
+            <p className="page-content-text">
+                Nu har du ditt livs bästa chans att ge tillbaka för de upplevelser, de kontakter och det personliga inflytande som kamratskapen under 
+                studietiden har bjudit dig på. Samtidigt försäkrar du att kommande generationer har möjligheten att få lika mycket av sin studietid som 
+                du fått. Vi välkomnar dig att bidra till den återstående miljonen så att TF kan förverkliga de attraktiva och ändamålsenliga medlemsutrymmen 
+                som krävs för att nationen ska vara livskraftig även i framtiden. Allas bidrag behövs!
+            </p>
+    
+            <p className="page-content-text">
+                <b>TF ämnar säkra den återstående miljonen under första halvan av detta år, för att försäkra att flytten 2024 blir smidig!</b>
             </p>
             </div>
         </div>
